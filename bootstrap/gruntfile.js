e1b0eda2c5e26e1a9540220a6ca8ece4f7ec2568
amanbis <amanb91@gmail.com>
2016-02-19 17:47:33 +0700

    Refactor gruntfile for formatting

--- bootstrap/gruntfile.js
+++ bootstrap/gruntfile.js
@@ -1,12 +1,44 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-uglify')
+	
 	grunt.initConfig({
+
 		uglify: {
 			my_target: {
 				files: {
 					'_/js/main.js': ['_/components/js/main.js']
 				} //files
 			} //my_target
-		} //uglify
+		}, //uglify
+
+		watch: {
+			files: ['_/components/js/*.js'],
+			tasks: ['uglify']
+		}, //watch
+
+		browserSync: {
+			bsFiles: {
+				src: [
+					'_/components/js/*.js',
+					'./*.html'
+				]
+			},
+			options: {
+				watchTask: true,
+				server: {
+					baseDir: './'
+				}
+			}
+		} //browser-sync
+
 	}) //initConfig
-} //exports
+
+	// load npm tasks
+	grunt.loadNpmTasks('grunt-contrib-uglify')
+	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-contrib-compass')
+	grunt.loadNpmTasks('grunt-browser-sync')
+
+	// define default task
+    grunt.registerTask('default', ['browserSync', 'watch'])
+
+} // exports
