248f8d538b03758780fec0a67135586f41b15ba1
amanbis <amanb91@gmail.com>
2016-03-04 23:30:56 +0700

    Add sticky product navigation

--- bootstrap/_/components/js/main.js
+++ bootstrap/_/components/js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+
+	/** 
+     * This part does the "fixed navigation after scroll" functionality
+     * We use the jQuery function scroll() to recalculate our variables as the 
+     * page is scrolled/
+     */
+    $(window).scroll(function() {
+    	var window_top = $(window).scrollTop()
+    	var div_top = $('header').offset().top
+
+    	if(window_top > div_top) {
+    		$('.product-nav').addClass('sticky')
+    	} else {
+    		$('.product-nav').removeClass('sticky')
+    	}
+    })
+
 	$('.feature').viewportChecker({
 		classToAdd: 'animate'
 	})
--- bootstrap/_/js/main.js
+++ bootstrap/_/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".feature").viewportChecker({classToAdd:"animate"})});
+$(document).ready(function(){$(window).scroll(function(){var a=$(window).scrollTop(),b=$("header").offset().top;a>b?$(".product-nav").addClass("sticky"):$(".product-nav").removeClass("sticky")}),$(".feature").viewportChecker({classToAdd:"animate"})});
