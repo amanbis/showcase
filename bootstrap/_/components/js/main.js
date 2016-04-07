d644cda11b42f5c268ece05e8319a92d7c170f26
amanbis <amanb91@gmail.com>
2016-04-07 11:29:44 +0700

    Make product menu toggle able for tablets and smaller
    Hover only works from 1200px up

--- bootstrap/_/components/js/main.js
+++ bootstrap/_/components/js/main.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
 
+    var $window = $(window)
+
+    $('.products-btn').click(function() {
+        if($window.width() > 992) {
+            $('.drawer-menu').toggleClass('show')
+        }
+    })
+
+    /*function toggleDrawer() {
+        return $('.products-btn').click(function() {
+            $('.drawer-menu').toggleClass('show')
+        })
+    }
+
+    function resize() {
+        if($window.width() > 992 && $window.width < 1200) {
+            return toggleDrawer()
+        }
+    }*/
+
 	/** 
      * This part does the "fixed navigation after scroll" functionality
      * We use the jQuery function scroll() to recalculate our variables as the 
      * page is scrolled/
      */
-    $(window).scroll(function() {
-    	var window_top = $(window).scrollTop()
-    	var div_top = $('header').offset().top
+    $window.scroll(function() {
+    	var window_top = $window.scrollTop(),
+    	    div_top = $('header').offset().top
 
     	if(window_top > div_top) {
     		$('.product-nav').addClass('sticky')
@@ -21,5 +41,9 @@ $(document).ready(function() {
 	$('.feature').viewportChecker({
 		classToAdd: 'animate'
 	})
+
+    /*$window
+        .resize(resize)
+        .trigger('resize')*/
 
 })
